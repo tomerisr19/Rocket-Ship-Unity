949484ebb8a9920398eef3638a69a5c04b68f755
tomerisr19 <tomerisr19@gmail.com>
2022-04-27 11:24:58 +0300

    second

---
+++ Rocket-Ship-2/Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject
+{
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] float moveSpeed = 5f;
+
+    public Transform GetStartingWayPoint()
+    {
+        return pathPrefab.GetChild(0);
+    }
+
+
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+}
