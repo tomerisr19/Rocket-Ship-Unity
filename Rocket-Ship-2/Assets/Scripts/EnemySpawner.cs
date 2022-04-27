89ce7a3443b415f0846801c05b8034c0eab80ee4
tomerisr19 <tomerisr19@gmail.com>
2022-04-27 21:31:52 +0300

    DamageDealer and Health

--- Rocket-Ship-2/Assets/Scripts/EnemySpawner.cs
+++ Rocket-Ship-2/Assets/Scripts/EnemySpawner.cs
@@ -8,7 +8,10 @@ public class EnemySpawner : MonoBehaviour
 
     [SerializeField] List<WaveConfigSO> waveConfigs;
     [SerializeField] float timeBetweenWaves = 0;
+    
     WaveConfigSO currentWave;
+
+    [SerializeField] bool isLooping = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,19 +25,20 @@ public class EnemySpawner : MonoBehaviour
     }
     IEnumerator SpawnEnemiesWaves()
     {
-       foreach(WaveConfigSO wave in waveConfigs)
+        do
         {
-            currentWave = wave;
-            for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+            foreach (WaveConfigSO wave in waveConfigs)
             {
-                Instantiate(currentWave.GetEnemyPrefab(i),
-                        currentWave.GetStartingWayPoint().position,
-                        Quaternion.identity, transform);
-                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                currentWave = wave;
+                for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+                {
+                    Instantiate(currentWave.GetEnemyPrefab(i),
+                            currentWave.GetStartingWayPoint().position,
+                            Quaternion.identity, transform);
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }
+                yield return new WaitForSeconds(timeBetweenWaves);
             }
-            yield return new WaitForSeconds(timeBetweenWaves);
-        }
-        
-        
+        } while (isLooping);                     
     }
 }
